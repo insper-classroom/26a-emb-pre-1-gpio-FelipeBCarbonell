5d20bbc5b3e998385c956cdd97b494fec60da709
Felipe Carbonell <fbcarbonell22@gmail.com>
2026-02-24 17:41:06 -0300

    faaah

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,18 +1,22 @@
 #include "stdio.h"
 
 
-void foo(int a) {
-    return = a + 1;
+void foo(int *a) {
+    return  a + 1;
 }
 
 void main(void) {
     int a = 0;
     int b = 0;
     while (1) {
-        foo();
+        foo(&a);
         
         if (a > 5) {
             b = 1;
         }
+    }
+
+    if (b == 1) {
+        printf("b = 1\n");
     }
 }
